b0482fc812abb09bbd68fc10c7044373c71945c7
maria-ys <ys.mariayounas@gmail.com>
2023-01-31 21:34:06 +0500

    basic structure

---
+++ controllers/productController.js
@@ -0,0 +1,15 @@
+const Product = require('../models/Product');
+const {StatusCodes} = require('http-status-codes');
+
+const createProduct = (req, res)=>{
+    res.send('create product');
+}
+
+const getAllProducts = (req, res)=>{
+    res.send('get all products');
+}
+
+module.exports = {
+    createProduct,
+    getAllProducts
+}
