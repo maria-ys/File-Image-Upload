58111abd43f247b178872494f84227bc5d0b586b
maria-ys <ys.mariayounas@gmail.com>
2023-02-04 11:24:01 +0500

    upload product image error handling

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,8 +1,19 @@
 const { StatusCodes } = require("http-status-codes");
+const CustomError = require('../errors');
 const path = require("path");
 const uploadProductImage = async (req, res) => {
+    if(!req.files){
+        throw new CustomError.BadRequestError('Please upload product image');
+    }
   const imageFile = req.files.image;
 
+  if(!imageFile.mimetype.startsWith('image')){
+    throw new CustomError.BadRequestError('Please upload an image');
+  }
+  const maxSize = 1024 * 1024;
+  if(imageFile.size > maxSize){
+    throw new CustomError.BadRequestError('Please upload image less than 1MB');
+  }
   const imagePath = path.join(
     __dirname,
     "../public/uploads/" + `${imageFile.name}`
